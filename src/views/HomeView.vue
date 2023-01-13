530c1171b2fab6d2144b0f7352428105b036e49f
Melvin-Jovano <melvinjovano2@gmail.com>
2023-01-13 15:25:56 +0700

    chore: add tailwind + flowbite

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,24 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+  import Carousel from '../components/Carousel.vue';
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 px-3 md:px-0">
+    <div class="py-5">
+      <Carousel />
+    </div>
+
+    <div class="py-5">
+      <h2 class="font-medium leading-tight text-3xl mt-0 mb-2">Simopen Lalin (Sistem Monitoring Pengatur Lalu Lintas)</h2>
+      
+      <p class="mt-5">
+        A Mobile And Web Application To Track Users With Given Location And Time Range.
+      </p>
+
+      <div class="mt-5">
+        <h3 class="font-medium leading-tight text-xl mt-0 mb-2">Technology Used:</h3>
+        Flutter, Node.js, Vue.js
+      </div>
+    </div>
+  </div>
 </template>
